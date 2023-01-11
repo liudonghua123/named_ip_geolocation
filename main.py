b106d5355bb41eb072c60e6bf99ae02947cdbac1
liudonghua123 <liudonghua123@gmail.com>
2023-01-11 10:57:42 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,195 @@
+import fire
+from xdbSearcher import XdbSearcher
+from os.path import join, dirname, exists
+from utils import init_logging, spinner_context, get_file_line_count
+from dataclasses import dataclass
+from collections.abc import Callable
+import re
+import yaml
+
+
+@dataclass
+class NamedQueryResult:
+    datetime: str
+    ip: str
+    view: str
+    domain: str
+    type: str
+    name_server: str
+
+    def from_dict(dict_value):
+        return NamedQueryResult(**dict_value)
+
+
+@dataclass
+class NamedQueryResultExtra:
+    ip: str
+    info: list[NamedQueryResult]
+    geolocation: dict
+
+
+@dataclass
+class Filter:
+    key: str
+    value: str
+    fn: Callable[[str, str, dict], bool]
+
+
+@dataclass
+class RecordItem:
+    ip: str
+    count: int
+    geolocation: dict
+    
+    def to_dict(self):
+        return {
+            'ip': self.ip,
+            'count': self.count,
+            'geolocation': self.geolocation
+        }
+
+@dataclass
+class Report:
+    domain: str
+    total: int
+    records: list[RecordItem]
+    
+    def to_dict(self):
+        return {
+            'domain': self.domain,
+            'total': self.total,
+            'records': list(map(lambda record: record.to_dict(), self.records))
+        }
+
+
+logger = init_logging()
+
+
+class SearchIPGeolocation:
+    _inner_cache = {}
+
+    def __init__(self, dbPath=join(dirname(__file__), 'data', 'ip2region.xdb')) -> None:
+        self.dbPath = dbPath
+        self.searcher = XdbSearcher(
+            contentBuff=XdbSearcher.loadContentFromFile(dbfile=self.dbPath))
+
+    def search(self, ip):
+        if ip not in self._inner_cache:
+            self._inner_cache[ip] = self.searcher.searchByIPStr(ip)
+        return self._inner_cache[ip]
+
+    def close(self):
+        self.searcher.close()
+
+
+class NamedQueryLogParser:
+
+    def __init__(self, query_log_path, filters: list[Filter] = []) -> None:
+        # check if the file exists
+        if not exists(query_log_path):
+            raise FileNotFoundError(f'File {query_log_path} not found')
+        self.query_log_path = query_log_path
+        self.filters = filters
+
+    def _filter(self, match_dict):
+        for filter in self.filters:
+            if not filter.fn(filter.key, filter.value, match_dict):
+                return False
+        return True
+
+    def parse(self):
+        regex = r'(?P<datetime>.*?) queries: info: client @[0-9a-fx]* (?P<ip>[0-9.:]*)#\d+ \(.*\): view (?P<view>\w+): query: (?P<domain>[\w.]+) IN (?P<type>\w+) .*? \((?P<name_server>[0-9.:]+)\)'
+        named_query_results: list[NamedQueryResult] = []
+        # calculate the file lines of the file_input
+        with spinner_context('Calculate file line count ...') as spinner:
+            file_input_lines = get_file_line_count(self.query_log_path)
+        logger.info(
+            f'file_input: {self.query_log_path}, lines: {file_input_lines}')
+        with spinner_context(f'Processing {self.query_log_path}...') as spinner, open(self.query_log_path, 'r') as f:
+            # update the spinner text to show the progress in 00.01% minimum
+            update_tick = int(file_input_lines /
+                              1000 if file_input_lines > 1000 else 100)
+            for index, line in enumerate(f):
+                match = re.search(regex, line)
+                if match:
+                    match_dict = match.groupdict()
+                    if self._filter(match_dict):
+                        logger.debug(f'Found matched log {match_dict}')
+                        named_query_results.append(
+                            NamedQueryResult.from_dict(match_dict))
+                if index % update_tick == 0:
+                    spinner.text = f'Processing {index / file_input_lines * 100:.2f}%'
+        logger.info(f'Found {len(named_query_results)} matched logs')
+        return named_query_results
+
+    def aggregate_by_source_ip(self, named_query_results: list[NamedQueryResult], search_ip_geolocation: SearchIPGeolocation):
+        # named_query_result_extras = list(map(lambda named_query_result: NamedQueryResultExtra(named_query_result.domain, named_query_result, search_ip_geolocation.search(
+        #     named_query_result.ip)), named_query_results))
+        named_query_result_extras: dict[str, NamedQueryResultExtra] = {}
+        for named_query_result in named_query_results:
+            if named_query_result.ip not in named_query_result_extras:
+                named_query_result_extras[named_query_result.ip] = NamedQueryResultExtra(
+                    named_query_result.ip, [named_query_result], search_ip_geolocation.search(named_query_result.ip))
+            else:
+                named_query_result_extras[named_query_result.ip].info.append(
+                    named_query_result)
+        results = [*named_query_result_extras.values()]
+        logger.info(
+            f'For {len(named_query_results)} matched logs, produced {len(results)} named_query_result_extras')
+        return results
+
+    def make_simple_report(self, named_query_result_extras: list[NamedQueryResultExtra]):
+        records = list(map(lambda named_query_result_extra: RecordItem(named_query_result_extra.ip, len(
+            named_query_result_extra.info), named_query_result_extra.geolocation), named_query_result_extras))
+        report = Report(self.filters[0].value, sum(map(lambda record: record.count, records)), records)
+        return report
+
+
+def _make_filters(filter_domain, fuzzy_search):
+    # make filters
+    filters = []
+    filter = Filter('domain', filter_domain,
+                    lambda key, value, match_dict: value in match_dict[key]
+                    if fuzzy_search else
+                    lambda key, value, match_dict: value == match_dict[key])
+    filters.append(filter)
+    return filters
+
+
+def main(query_log_path=join(dirname(__file__), 'test.log'),
+         dbPath=join(dirname(__file__), 'data', 'ip2region.xdb'),
+         filter_domain='www.jwc.ynu.edu.cn',
+         report_path=join(dirname(__file__), 'report.yaml'),
+         fuzzy_search=False,):
+    search_ip_geolocation = SearchIPGeolocation(dbPath)
+    filters = _make_filters(filter_domain, fuzzy_search)
+    named_query_log_parser = NamedQueryLogParser(query_log_path, filters)
+    named_query_results = named_query_log_parser.parse()
+    named_query_result_extras = named_query_log_parser.aggregate_by_source_ip(
+        named_query_results, search_ip_geolocation)
+    report = named_query_log_parser.make_simple_report(
+        named_query_result_extras)
+    with open(report_path, 'w', encoding='utf-8') as yamlfile:
+        # https://matthewpburruss.com/post/yaml/
+        # convert the report object to dict then dump to yaml to avoid the yaml tag
+        yaml.dump(report.to_dict(), yamlfile, sort_keys=False,
+                  indent=2, allow_unicode=True)
+        logger.info(f'write report: {report_path}')
+    # with open(report_path, 'r', encoding='utf-8') as yamlfile:
+    #     # https://matthewpburruss.com/post/yaml/
+    #     loader = yaml.SafeLoader
+    #     def report_loader(loader, node):
+    #         return Report(**loader.construct_mapping(node, deep=True))
+
+    #     def report_item_loadder(loader, node):
+    #         return RecordItem(**loader.construct_mapping(node, deep=True))
+    #     loader.add_constructor("!Report", report_loader)
+    #     loader.add_constructor("!ReportItem", report_item_loadder)
+    #     parsed_report = yaml.load(yamlfile, Loader=loader)
+    #     logger.info(
+    #         f'read report: {report_path}, parsed_report: {parsed_report}')
+
+
+if __name__ == '__main__':
+    fire.core.Display = lambda lines, out: print(*lines, file=out)
+    fire.Fire(main)
---
+++ utils.py
@@ -0,0 +1,45 @@
+import time
+from halo import Halo
+import logging
+import sys
+import os
+from os.path import dirname, join, realpath, basename
+
+# config logging for both stdout and file
+level=os.environ.get("LOG_LEVEL", "INFO")
+
+def init_logging(file_name=join(dirname(realpath(__file__)), "app.log"), level=level):
+    logging.basicConfig(
+        format="%(levelname)s %(asctime)s - %(message)s",
+        level=level,
+        handlers=[logging.FileHandler(file_name), logging.StreamHandler(sys.stdout)],
+    )
+    logger = logging.getLogger(basename(file_name))
+    return logger
+
+
+class spinner_context:
+    def __init__(self, start_text: str, end_text: str = None, spinner_indicator: str = 'dots'):
+        self.start_text = start_text
+        self.end_text = end_text or start_text
+        self.spinner_indicator = spinner_indicator
+        self.spinner = Halo(text=self.start_text, spinner=self.spinner_indicator)
+        self.start_time = None
+    def __enter__(self):
+        self.start_time = time.perf_counter()
+        self.spinner.start()
+        return self.spinner
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.spinner.succeed(f'{self.end_text}, took {time.perf_counter() - self.start_time:.2f}s')
+
+def get_file_line_count(file_path):
+    with open(file_path, 'rb') as fp:
+        def _read(reader):
+            buffer_size = 1024 * 1024
+            b = reader(buffer_size)
+            while b:
+                yield b
+                b = reader(buffer_size)
+        content_generator = _read(fp.raw.read)
+        count = sum(buffer.count(b'\n') for buffer in content_generator)
+        return count
